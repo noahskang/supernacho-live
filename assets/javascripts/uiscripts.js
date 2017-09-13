abda5f4c98a7e71f4931e08cf0dd3e8cdfe3dd51
Noah Kang <noahskang@gmail.com>
2017-09-13 11:32:01 -0700

    Remove console.log

--- assets/javascripts/uiscripts.js
+++ assets/javascripts/uiscripts.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
   $('.icon').on('click', function(e){
-    console.log("detected");
     e.preventDefault();
     var url = $(this).data('url');
     window.location.replace(url);
